4e21bcf91b5ad541acdf236274898eec29f18c36
Alive <appreach_ing@Hotmail.com>
2019-04-20 23:36:33 +0800

    bump es6

--- index.js
+++ index.js
@@ -1,155 +1,153 @@
-import easingTypes, {easeInOutQuad} from 'tween-functions';
-import requestAnimationFrame from 'raf';
+'use strict';
+
+const easingTypes = require('tween-functions');
 
 // additive is the new iOS 8 default. In most cases it simulates a physics-
 // looking overshoot behavior (especially with easeInOut. You can test that in
 // the example
 const DEFAULT_STACK_BEHAVIOR = 'ADDITIVE';
-const DEFAULT_EASING = easeInOutQuad;
+const DEFAULT_EASING = easingTypes.easeInOutQuad;
 const DEFAULT_DURATION = 300;
 const DEFAULT_DELAY = 0;
 
-const stackBehavior = {
-  ADDITIVE: 'ADDITIVE',
-  DESTRUCTIVE: 'DESTRUCTIVE',
+// see usage below
+function returnState(state) {
+  return state;
+}
+
+const tweenState = {
+  easingTypes: easingTypes,
+  stackBehavior: {
+    ADDITIVE: 'ADDITIVE',
+    DESTRUCTIVE: 'DESTRUCTIVE',
+  }
 };
 
-const Mixin = {
-  _rafID: null,
+class Mixin {
+  constructor() {
+    this.state = { tweenQueue: [] };
+  }
 
-  getInitialState() {
-    return {
-      tweenQueue: [],
-    };
-  },
+  tweenState = (a, b, c) => {
+    // tweenState(stateNameString, config)
+    // tweenState(stateRefFunc, stateNameString, config)
 
-  componentWillUnmount() {
-    requestAnimationFrame.cancel(this._rafID);
-    this._rafID = -1;
-  },
-
-  tweenState(path, {easing, duration, delay, beginValue, endValue, onEnd, stackBehavior: configSB}) {
-    this.setState(state => {
-      let cursor = state;
-      let stateName;
-      // see comment below on pash hash
-      let pathHash;
-      if (typeof path === 'string') {
-        stateName = path;
-        pathHash = path;
-      } else {
-        for (let i = 0; i < path.length - 1; i++) {
-          cursor = cursor[path[i]];
-        }
-        stateName = path[path.length - 1];
-        pathHash = path.join('|');
-      }
-      // see the reasoning for these defaults at the top of file
-      const newConfig = {
-        easing: easing || DEFAULT_EASING,
-        duration: duration == null ? DEFAULT_DURATION : duration,
-        delay: delay == null ? DEFAULT_DELAY : delay,
-        beginValue: beginValue == null ? cursor[stateName] : beginValue,
-        endValue: endValue,
-        onEnd: onEnd,
-        stackBehavior: configSB || DEFAULT_STACK_BEHAVIOR,
-      };
-
-      let newTweenQueue = state.tweenQueue;
-      if (newConfig.stackBehavior === stackBehavior.DESTRUCTIVE) {
-        newTweenQueue = state.tweenQueue.filter(item => item.pathHash !== pathHash);
-      }
-
-      // we store path hash, so that during value retrieval we can use hash
-      // comparison to find the path. See the kind of shitty thing you have to
-      // do when you don't have value comparison for collections?
-      newTweenQueue.push({
-        pathHash: pathHash,
-        config: newConfig,
-        initTime: Date.now() + newConfig.delay,
-      });
-
-      // sorry for mutating. For perf reasons we don't want to deep clone.
-      // guys, can we please all start using persistent collections so that
-      // we can stop worrying about nonesense like this
-      cursor[stateName] = newConfig.endValue;
-      if (newTweenQueue.length === 1) {
-        this._rafID = requestAnimationFrame(this._rafCb.bind(this));
-      }
-
-      // this will also include the above mutated update
-      return {tweenQueue: newTweenQueue};
-    });
-  },
-
-  getTweeningValue(path) {
-    const state = this.state;
-
-    let tweeningValue;
-    let pathHash;
-    if (typeof path === 'string') {
-      tweeningValue = state[path];
-      pathHash = path;
-    } else {
-      tweeningValue = state;
-      for (let i = 0; i < path.length; i++) {
-        tweeningValue = tweeningValue[path[i]];
-      }
-      pathHash = path.join('|');
+    // passing a state name string and retrieving it later from this.state
+    // doesn't work for values in deeply nested collections (unless you design
+    // the API to be able to parse 'this.state.my.nested[1]', meh). Passing a
+    // direct, resolved reference wouldn't work either, since that reference
+    // points to the old state rather than the subsequent new ones.
+    if (typeof a === 'string') {
+      c = b;
+      b = a;
+      a = returnState;
     }
-    let now = Date.now();
+    this._tweenState(a, b, c);
+  }
 
-    for (let i = 0; i < state.tweenQueue.length; i++) {
-      const {pathHash: itemPathHash, initTime, config} = state.tweenQueue[i];
-      if (itemPathHash !== pathHash) {
+  _tweenState = (stateRefFunc, stateName, config) => {
+    // _pendingState doesn't exist in React 0.13 anymore. No harm leaving it
+    // here for backward compat
+    const state = this._pendingState || this.state;
+    const stateRef = stateRefFunc(state);
+
+    // see the reasoning for these defaults at the top
+    const newConfig = {
+      easing: config.easing || DEFAULT_EASING,
+      duration: config.duration == null ? DEFAULT_DURATION : config.duration,
+      delay: config.delay == null ? DEFAULT_DELAY : config.delay,
+      beginValue: config.beginValue == null ? stateRef[stateName] : config.beginValue,
+      endValue: config.endValue,
+      onEnd: config.onEnd,
+      stackBehavior: config.stackBehavior || DEFAULT_STACK_BEHAVIOR,
+    };
+
+    const newTweenQueue = state.tweenQueue;
+    if (newConfig.stackBehavior === tweenState.stackBehavior.DESTRUCTIVE) {
+      newTweenQueue = state.tweenQueue.filter(function (item) {
+        return item.stateName !== stateName || item.stateRefFunc(state) !== stateRef;
+      });
+    }
+
+    newTweenQueue.push({
+      stateRefFunc: stateRefFunc,
+      stateName: stateName,
+      config: newConfig,
+      initTime: Date.now() + newConfig.delay,
+    });
+
+    // tweenState calls setState
+    // sorry for mutating. No idea where in the state the value is
+    stateRef[stateName] = newConfig.endValue;
+    // this will also include the above update
+    this.setState({ tweenQueue: newTweenQueue });
+
+    if (newTweenQueue.length === 1) {
+      this.startRaf();
+    }
+  }
+
+  getTweeningValue = (a, b) => {
+    // see tweenState API
+    if (typeof a === 'string') {
+      b = a;
+      a = returnState;
+    }
+    return this._getTweeningValue(a, b);
+  }
+
+  _getTweeningValue = (stateRefFunc, stateName) => {
+    const state = this.state;
+    const stateRef = stateRefFunc(state);
+    const tweeningValue = stateRef[stateName];
+    const now = Date.now();
+
+    for (const i = 0; i < state.tweenQueue.length; i++) {
+      const item = state.tweenQueue[i];
+      const itemStateRef = item.stateRefFunc(state);
+      if (item.stateName !== stateName || itemStateRef !== stateRef) {
         continue;
       }
 
-      const progressTime = now - initTime > config.duration
-        ? config.duration
-        : Math.max(0, now - initTime);
-      // `now - initTime` can be negative if initTime is scheduled in the
+      const progressTime = now - item.initTime > item.config.duration ?
+        item.config.duration :
+        Math.max(0, now - item.initTime);
+      // `now - item.initTime` can be negative if initTime is scheduled in the
       // future by a delay. In this case we take 0
 
-      // if duration is 0, consider that as jumping to endValue directly. This
-      // is needed because the easing functino might have undefined behavior for
-      // duration = 0
-      const easeValue = config.duration === 0 ? config.endValue : config.easing(
+      const contrib = -item.config.endValue + item.config.easing(
         progressTime,
-        config.beginValue,
-        config.endValue,
-        config.duration,
+        item.config.beginValue,
+        item.config.endValue,
+        item.config.duration
         // TODO: some funcs accept a 5th param
       );
-      const contrib = easeValue - config.endValue;
       tweeningValue += contrib;
     }
 
     return tweeningValue;
-  },
+  }
 
-  _rafCb() {
+  _rafCb = () => {
     const state = this.state;
     if (state.tweenQueue.length === 0) {
       return;
     }
 
     const now = Date.now();
-    let newTweenQueue = [];
+    const newTweenQueue = [];
 
-    for (let i = 0; i < state.tweenQueue.length; i++) {
+    for (const i = 0; i < state.tweenQueue.length; i++) {
       const item = state.tweenQueue[i];
-      const {initTime, config} = item;
-      if (now - initTime < config.duration) {
+      if (now - item.initTime < item.config.duration) {
         newTweenQueue.push(item);
       } else {
-        config.onEnd && config.onEnd();
+        item.config.onEnd && item.config.onEnd();
       }
     }
 
     // onEnd might trigger a parent callback that removes this component
-    // -1 means we've canceled it in componentWillUnmount
-    if (this._rafID === -1) {
+    if (!this.isMounted()) {
       return;
     }
 
@@ -157,12 +155,14 @@ const Mixin = {
       tweenQueue: newTweenQueue,
     });
 
-    this._rafID = requestAnimationFrame(this._rafCb.bind(this));
-  },
-};
+    requestAnimationFrame(this._rafCb);
+  }
 
-export default {
-  Mixin,
-  easingTypes,
-  stackBehavior,
-};
+  startRaf = () => {
+    requestAnimationFrame(this._rafCb);
+  }
+}
+
+tweenState.Mixin = Mixin;
+
+module.exports = tweenState;
